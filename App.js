b4d15ea6ec50f311d5f4acd8ce80e7e5d07f85df
raul fernandez <raulpoolan@gmail.com>
2022-10-04 10:05:20 +0300

    2nd try

--- App.js
+++ App.js
@@ -1,58 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
-import * as SQLite from 'expo-sqlite';
-import { initializeApp } from'firebase/app';
-import { getDatabase, push, ref, onValue } from'firebase/database';
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  FlatList,
+} from "react-native";
+import { initializeApp } from "firebase/app";
+import { getDatabase, push, ref, onValue } from "firebase/database";
 
 export default function App() {
-
-  const [product, setProduct] = useState('');
-  const [amount, setAmount] = useState('');
-  const [list, setList] = useState([]);
+  const [product, setProduct] = useState("");
+  const [amount, setAmount] = useState("");
+  const [items, setItems] = useState([]);
+  const app = initializeApp(firebaseConfig);
+  const database = getDatabase(app);
 
   const firebaseConfig = {
     apiKey: "AIzaSyCmH3YywfCGazVqBD0WD9vTSbZkJcp16yw",
     authDomain: "poolanproject.firebaseapp.com",
-    databaseURL: "https://poolanproject-default-rtdb.europe-west1.firebasedatabase.app",
+    databaseURL:
+      "https://poolanproject-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "poolanproject",
     storageBucket: "poolanproject.appspot.com",
     messagingSenderId: "658229132912",
-    appId: "1:658229132912:web:9359e86bd3da03abbcbeb5"
+    appId: "1:658229132912:web:9359e86bd3da03abbcbeb5",
   };
-  
-  const app = initializeApp(firebaseConfig); 
-  const database = getDatabase(app);
 
-  ref(database,'items/')
+  ref(database, "items/");
 
   useEffect(() => {
-    db.transaction(tx => {
-      tx.executeSql('create table if not exists shoppingList (id integer primary key not null, product text, amount text);');
-    }, null, updateList); 
+    const itemsRef = ref(database, "items/");
+    onValue(itemsRef, (snapshot) => {
+      const data = snapshot.val();
+      setItems(Object.values(data));
+    });
   }, []);
 
   const saveItem = () => {
-    db.transaction(tx => {
-        tx.executeSql('insert into shoppingList (product, amount) values (?, ?);', [product, amount]);    
-      }, null, updateList
-    )
-  }
-
-  const updateList = () => {
-    db.transaction(tx => {
-      tx.executeSql('select * from shoppingList;', [], (_, { rows }) =>
-        setList(rows._array)
-      ); 
-    });
-  }
-
-  const deleteItem = (id) => {
-    db.transaction(
-      tx => {
-        tx.executeSql(`delete from shoppingList where id = ?;`, [id]);
-      }, null, updateList
-    )    
-  }
+    push(ref(database, "items/"), { product: product, amount: amount });
+    setProduct("");
+    setAmount("");
+  };
 
   const listSeparator = () => {
     return (
@@ -69,36 +59,67 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <TextInput placeholder='product' style={{marginTop: 30, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
+      <TextInput
+        placeholder="product"
+        style={{
+          marginTop: 30,
+          fontSize: 18,
+          width: 200,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
         onChangeText={(product) => setProduct(product)}
-        value={product}/>  
-      <TextInput placeholder='amounts' style={{ marginTop: 5, marginBottom: 5,  fontSize:18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        value={product}
+      />
+      <TextInput
+        placeholder="amounts"
+        style={{
+          marginTop: 5,
+          marginBottom: 5,
+          fontSize: 18,
+          width: 200,
+          borderColor: "gray",
+          borderWidth: 1,
+        }}
         onChangeText={(amount) => setAmount(amount)}
-        value={amount}/>      
-      <Button onPress={saveItem} title="Save" /> 
-      <Text style={{marginTop: 30, fontSize: 20}}>Shopping List</Text>
-      <FlatList 
-        style={{marginLeft : "5%"}}
-        keyExtractor={item => item.id.toString()} 
-        renderItem={({item}) => <View style={styles.listcontainer}><Text style={{fontSize: 18}}>{item.product}, {item.amount}</Text>
-        <Text style={{fontSize: 18, color: '#0000ff'}} onPress={() => deleteItem(item.id)}> Done</Text></View>} 
-        data={list} 
-        ItemSeparatorComponent={listSeparator} 
-      />      
+        value={amount}
+      />
+      <Button onPress={saveItem} title="Save" />
+      <Text style={{ marginTop: 30, fontSize: 20 }}>Shopping List</Text>
+      <FlatList
+        style={{ marginLeft: "5%" }}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.listcontainer}>
+            <Text style={{ fontSize: 18 }}>
+              {item.product}, {item.amount}
+            </Text>
+            <Text
+              style={{ fontSize: 18, color: "#0000ff" }}
+              onPress={() => deleteItem(item.id)}
+            >
+              {" "}
+              Done
+            </Text>
+          </View>
+        )}
+        data={items}
+        ItemSeparatorComponent={listSeparator}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
- container: {
-  flex: 1,
-  backgroundColor: '#fff',
-  alignItems: 'center',
-  justifyContent: 'center',
- },
- listcontainer: {
-  flexDirection: 'row',
-  backgroundColor: '#fff',
-  alignItems: 'center'
- },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  listcontainer: {
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+  },
 });
